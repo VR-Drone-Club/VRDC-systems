d07fcf4c13b7c8be09ebfd995c5ba0bc080da75b
Phasedragon <carter@fansler.us>
2025-03-27 00:36:16 -0700

    Initial WIP gate system
    Not very functional yet, just committing this just to keep track of progress

---
+++ Gates/_scripts/DroneGate.cs
@@ -0,0 +1,47 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public enum GateState
+{
+    Idle,
+    EncourageEntry,
+    DiscourageEntry,
+}
+public class DroneGate : UdonSharpBehaviour
+{
+    public GameObject idleEffects;
+    public GameObject encourageEffects;
+    public GameObject discourageEffects;
+    
+    private GateConnector _connector;
+
+    private GateState _state;
+    public GateState State
+    {
+        get
+        {
+            return _state;
+        }
+        set
+        {
+            _state = value;
+            if (Utilities.IsValid(encourageEffects)) encourageEffects.SetActive(value == GateState.EncourageEntry);
+            if (Utilities.IsValid(discourageEffects)) discourageEffects.SetActive(value == GateState.DiscourageEntry);
+            if (Utilities.IsValid(idleEffects)) idleEffects.SetActive(value == GateState.Idle);
+        }
+    }
+    
+    public void RegisterConnector(GateConnector connector)
+    {
+        _connector = connector; // If this script is visible by a GateConnector, it should reach out and tell it where it belongs. This makes that connection happen.
+    }
+    
+    public override void OnDroneTriggerEnter(VRCDroneApi drone)
+    {
+        if (Utilities.IsValid(_connector)) _connector.GateTriggered(this); // Pass events along to the GateConnector, if there is one.
+    }
+    
+}
---
+++ Gates/_scripts/GateConnector.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class GateConnector : UdonSharpBehaviour
+{
+    public DroneGate[] gates;
+
+    private void Start()
+    {
+        if (!Utilities.IsValid(gates)) return;
+        foreach (var gate in gates)
+        {
+            gate.RegisterConnector(this);
+        }
+    }
+
+    public void GateTriggered(DroneGate gate)
+    {
+        if (!Utilities.IsValid(gate)) return;
+        int index = Array.IndexOf(gates, gate);
+        if (index == -1) return;
+    }
+}
