c2f94fbf2e3cb7ad8a8732317ff4303e838b0457
Phasedragon <24859894+Phasedragon@users.noreply.github.com>
2025-04-09 01:58:28 -0700

    Added sound effects to the railgun, all local for now

--- Launchers/_scripts/Railgun.cs
+++ Launchers/_scripts/Railgun.cs
@@ -11,10 +11,17 @@ public class Railgun : UdonSharpBehaviour
 {
     public Transform attachmentPoint;
     public Animator animator;
-    public AnimationCurve chargeCurve;
+    public AudioSource audioSource;
+    public AudioClip chargeSound;
+    public AudioClip launchSound;
+    public AudioClip launchSound2;
+    public float maxChargeTime;
+    public float minChargeVelocity;
+    public float maxChargeVelocity;
     
     private VRCPickup attachedPickup;
     private float chargeStartTime;
+    private float lastLaunchTime;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -24,30 +31,44 @@ public class Railgun : UdonSharpBehaviour
         VRCPickup pickup = other.GetComponent<VRCPickup>();
         if (!Utilities.IsValid(pickup)) return;
         if (!Networking.IsOwner(pickup.gameObject)) return;
+        if (lastLaunchTime + 0.5f > Time.timeSinceLevelLoad) return;
         attachedPickup = pickup;
     }
-
+    
     private void Update()
     {
-        if (!Networking.IsOwner(gameObject)) return;
-        if (!Utilities.IsValid(attachedPickup)) return;
+        if (!Networking.IsOwner(gameObject))
+        {
+            audioSource.clip = null;
+            return;
+        }
+
+        if (!Utilities.IsValid(attachedPickup))
+        {
+            audioSource.clip = null;
+            return;
+        }
         if (!Networking.IsOwner(attachedPickup.gameObject))
         {
+            audioSource.clip = null;
             attachedPickup = null;
             return;
         }
         attachedPickup.transform.position = attachmentPoint.position;
         attachedPickup.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        float chargeProgress = chargeStartTime == 0 ? 0 : (Time.timeSinceLevelLoad - chargeStartTime);
-        chargeProgress = Mathf.InverseLerp(0, chargeCurve.length, chargeProgress);
+        float chargeProgress = chargeStartTime == 0 ? 0 : Mathf.InverseLerp(chargeStartTime, chargeStartTime + maxChargeTime, Time.timeSinceLevelLoad);
         animator.SetFloat("ChargeProgress", chargeProgress);
+        audioSource.volume = Mathf.InverseLerp(1, 0.8f, chargeProgress); //should have full volume for most of it, then fade quickly at the end
     }
-
+    
     public override void OnPickupUseDown()
     {
         chargeStartTime = Time.timeSinceLevelLoad;
+        audioSource.clip = chargeSound;
+        audioSource.volume = 1;
+        audioSource.Play();
     }
-
+    
     public override void OnOwnershipTransferred(VRCPlayerApi player)
     {
         attachedPickup = null;
@@ -56,10 +77,13 @@ public class Railgun : UdonSharpBehaviour
     public override void OnPickupUseUp()
     {
         if (!Utilities.IsValid(attachedPickup)) return;
-        float chargeProgress = chargeStartTime == 0 ? 0 : Time.timeSinceLevelLoad - chargeStartTime;
+        float chargeProgress = chargeStartTime == 0 ? 0 : Mathf.InverseLerp(chargeStartTime, chargeStartTime + maxChargeTime, Time.timeSinceLevelLoad);
         chargeStartTime = 0;
-        attachedPickup.GetComponent<Rigidbody>().velocity = attachmentPoint.forward * chargeCurve.Evaluate(chargeProgress);
+        lastLaunchTime = Time.timeSinceLevelLoad;
+        attachedPickup.GetComponent<Rigidbody>().velocity = attachmentPoint.forward * Mathf.Lerp(minChargeVelocity, maxChargeVelocity, chargeProgress);
         attachedPickup = null;
         animator.SetFloat("ChargeProgress", 0);
+        audioSource.volume = 1;
+        audioSource.PlayOneShot(maxChargeTime < 10 ? launchSound : launchSound2, chargeProgress + 0.5f);
     }
 }
