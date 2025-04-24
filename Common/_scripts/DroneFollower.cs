f276fbfdc3932337fc90f6f28b5ea2454ae2a791
Phasedragon <24859894+Phasedragon@users.noreply.github.com>
2025-04-23 23:03:50 -0700

    Added DroneFollower

--- Common/_scripts/DroneFollower.cs
+++ Common/_scripts/DroneFollower.cs
@@ -6,8 +6,25 @@ using VRC.Udon;
 
 public class DroneFollower : UdonSharpBehaviour
 {
+    public Transform followObject;
+    private VRCDroneApi _drone;
     void Start()
     {
-        
+        _drone = Networking.GetOwner(gameObject).GetDrone();
+        Loop();
+    }
+
+    public void Loop()
+    {
+        if (!Utilities.IsValid(_drone)) return;
+        if (!_drone.IsDeployed())
+        {
+            SendCustomEventDelayedSeconds(nameof(Loop), 1);
+            followObject.gameObject.SetActive(false);
+            return;
+        }
+        followObject.gameObject.SetActive(true);
+        SendCustomEventDelayedSeconds(nameof(Loop), 0);
+        followObject.SetPositionAndRotation(_drone.GetPosition(), Quaternion.LookRotation(_drone.GetVelocity()));
     }
 }
